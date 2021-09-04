4284aeff3bb00916029358db59d73a9f0dcf6976
Tobias Maugus <tobiasmaugus705@gmail.com>
2021-09-04 16:41:51 -0300

    arrumando links

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,7 @@ import {Forum} from 'pages';
 
 const Routes = () =>{
     return (<Switch>
-        <Route path="/" component = {Home} />;
+        <Route path="//" component = {Home} />;
         <Route path="/suporte" component = {Suporte} />
         <Route path="/forum" component = {Forum} />
     </Switch>)
