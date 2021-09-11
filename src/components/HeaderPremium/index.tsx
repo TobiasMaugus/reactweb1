f5990bdc9616abbf1db3542b54f63c5e59cdab72
Tobias Maugus <tobiasmaugus705@gmail.com>
2021-09-10 21:35:34 -0300

    logo

--- src/components/HeaderPremium/index.tsx
+++ src/components/HeaderPremium/index.tsx
@@ -5,7 +5,7 @@ const HeaderPremium = () => {
     return(<S.HeaderPremium> 
         <h1 className="anime">TM ANIMES</h1>
         <picture className="logo">
-            <img src={logo} alt="Logo TM" width="160" height="124" />
+            <img src={logo} alt="Logo TM" width="150" height="120"  />
         </picture>
         <S.A href ="/" target="_self">
             <h1 className="premium">Home</h1>
